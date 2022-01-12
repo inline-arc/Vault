70303d650cda4c0b2672072b36b116203839d9d7
Your Name <karel.haerens@ml6.eu>
2022-01-12 02:11:44 +0100

    space

--- app.py
+++ app.py
@@ -1,4 +1,122 @@
+from logging import disable
+from pkg_resources import EggMetadata
 import streamlit as st
+import streamlit.components.v1 as components
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
+from streamlit.state.session_state import SessionState
+from streamlit.type_util import Key
+import rebel
+import wikipedia
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+network_filename = "test.html"
+
+state_variables = {
+    'has_run':False,
+    'wiki_suggestions': "",
+    'wiki_text' : [],
+    'nodes':[]
+}
+
+for k, v in state_variables.items():
+    if k not in st.session_state:
+        st.session_state[k] = v
+
+def clip_text(t, lenght = 5):
+    return ".".join(t.split(".")[:lenght]) + "."
+
+
+
+def generate_graph():
+    if 'wiki_text' not in st.session_state:
+        return
+    if len(st.session_state['wiki_text']) == 0:
+        st.error("please enter a topic and select a wiki page first")
+        return
+    with st.spinner(text="Generating graph..."):
+        texts = st.session_state['wiki_text']
+        nodes = rebel.generate_knowledge_graph(texts, network_filename)
+        st.session_state['nodes'] = nodes
+        st.session_state['has_run'] = True
+    st.success('Done!')
+
+def show_suggestion():
+    with st.spinner(text="fetching wiki topics..."):
+        if st.session_state['input_method'] == "wikipedia":
+            text = st.session_state.text
+            if text is not None:
+                st.session_state['wiki_suggestions'] = wikipedia.search(text, results = 3)
+
+def show_wiki_text(page_title):
+    with st.spinner(text="fetching wiki page..."):
+        try:
+            page = wikipedia.page(title=page_title, auto_suggest=False)
+            st.session_state['wiki_text'].append(clip_text(page.summary))
+        except wikipedia.DisambiguationError as e:
+            with st.spinner(text="Woops, ambigious term, recalculating options..."):
+                st.session_state['wiki_suggestions'].remove(page_title)
+                temp = st.session_state['wiki_suggestions'] + e.options[:3]
+                st.session_state['wiki_suggestions'] = list(set(temp))
+
+def add_text(term):
+    try:
+        extra_text = clip_text(wikipedia.page(title=term, auto_suggest=True).summary)
+        st.session_state['wiki_text'].append(extra_text)
+    except wikipedia.DisambiguationError as e:
+        st.session_state["nodes"].remove(term)
+
+
+def reset_session():
+    for k in state_variables:
+        del st.session_state[k]
+
+st.title('REBELious knowledge graph generation')
+st.session_state['input_method'] = "wikipedia"
+
+# st.selectbox(
+#      'input method',
+#      ('wikipedia', 'free text'),  key="input_method")
+
+if st.session_state['input_method'] != "wikipedia":
+    st.text_area("Your text", key="text")
+else:
+    st.text_input("wikipedia search term",on_change=show_suggestion, key="text")
+
+if len(st.session_state['wiki_suggestions']) != 0:
+    columns = st.columns([1] * len(st.session_state['wiki_suggestions']))
+    for i, (c, s) in enumerate(zip(columns, st.session_state['wiki_suggestions'])):
+        with c:
+            st.button(s, on_click=show_wiki_text, args=(s,), key=i)
+
+if len(st.session_state['wiki_text']) != 0:
+    for t in st.session_state['wiki_text']:
+        new_expander = st.expander(label=t[:30] + "...")
+        with new_expander:
+            st.markdown(t)
+
+if st.session_state['input_method'] != "wikipedia":
+    st.button("find wiki pages")
+    if "wiki_suggestions" in st.session_state:
+            st.button("generate", on_click=generate_graph, key="gen_graph")
+
+else:
+    st.button("generate", on_click=generate_graph, key="gen_graph2")
+
+
+if st.session_state['has_run']:
+    cols = st.columns([4, 1])
+    with cols[0]:
+        HtmlFile = open(network_filename, 'r', encoding='utf-8')
+        source_code = HtmlFile.read()
+        components.html(source_code, height=1500,width=1500)
+    with cols[1]:
+        st.text("expand")
+        for s in st.session_state["nodes"]:
+            st.button(s, on_click=add_text, args=(s,))
+
+
+
+
+
+
---
+++ rebel.py
@@ -0,0 +1,119 @@
+from typing import List
+from transformers import pipeline
+from pyvis.network import Network
+from functools import lru_cache
+from app import generate_graph
+import spacy
+from spacy import displacy
+
+
+DEFAULT_LABEL_COLORS = {
+    "ORG": "#7aecec",
+    "PRODUCT": "#bfeeb7",
+    "GPE": "#feca74",
+    "LOC": "#ff9561",
+    "PERSON": "#aa9cfc",
+    "NORP": "#c887fb",
+    "FACILITY": "#9cc9cc",
+    "EVENT": "#ffeb80",
+    "LAW": "#ff8197",
+    "LANGUAGE": "#ff8197",
+    "WORK_OF_ART": "#f0d0ff",
+    "DATE": "#bfe1d9",
+    "TIME": "#bfe1d9",
+    "MONEY": "#e4e7d2",
+    "QUANTITY": "#e4e7d2",
+    "ORDINAL": "#e4e7d2",
+    "CARDINAL": "#e4e7d2",
+    "PERCENT": "#e4e7d2",
+}
+
+def generate_knowledge_graph(texts: List[str], filename: str):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp("\n".join(texts))
+    NERs = [ent.text for ent in doc.ents]
+    NER_types =  [ent.label_ for ent in doc.ents]
+    for nr, nrt in zip(NERs, NER_types):
+        print(nr, nrt)
+
+    triplets = []
+    for triplet in texts:
+        triplets.extend(generate_partial_graph(triplet))
+    print(generate_partial_graph.cache_info())
+    heads = [ t["head"] for t in triplets]
+    tails = [ t["tail"] for t in triplets]
+
+    nodes = set(heads + tails)
+    net = Network(directed=True)
+
+    for n in nodes:
+        if n in NERs:
+            NER_type = NER_types[NERs.index(n)]
+            color = DEFAULT_LABEL_COLORS[NER_type]
+            net.add_node(n, title=NER_type, shape="circle", color=color)
+        else:
+            net.add_node(n, shape="circle")
+
+    unique_triplets = set()
+    stringify_trip = lambda x : x["tail"] + x["head"] + x["type"]
+    for triplet in triplets:
+        if stringify_trip(triplet) not in unique_triplets:
+            net.add_edge(triplet["tail"], triplet["head"], title=triplet["type"], label=triplet["type"])
+            unique_triplets.add(stringify_trip(triplet))
+
+    net.repulsion(
+        node_distance=200,
+        central_gravity=0.2,
+        spring_length=200,
+        spring_strength=0.05,
+        damping=0.09
+    )
+    net.set_edge_smooth('dynamic')
+    net.show(filename)
+    return nodes
+
+
+@lru_cache
+def generate_partial_graph(text):
+    triplet_extractor = pipeline('text2text-generation', model='Babelscape/rebel-large', tokenizer='Babelscape/rebel-large')
+    a = triplet_extractor(text, return_tensors=True, return_text=False)[0]["generated_token_ids"]["output_ids"]
+    extracted_text = triplet_extractor.tokenizer.batch_decode(a)
+    extracted_triplets = extract_triplets(extracted_text[0])
+    return extracted_triplets
+
+
+def extract_triplets(text):
+    """
+    Function to parse the generated text and extract the triplets
+    """
+    triplets = []
+    relation, subject, relation, object_ = '', '', '', ''
+    text = text.strip()
+    current = 'x'
+    for token in text.replace("<s>", "").replace("<pad>", "").replace("</s>", "").split():
+        if token == "<triplet>":
+            current = 't'
+            if relation != '':
+                triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+                relation = ''
+            subject = ''
+        elif token == "<subj>":
+            current = 's'
+            if relation != '':
+                triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+            object_ = ''
+        elif token == "<obj>":
+            current = 'o'
+            relation = ''
+        else:
+            if current == 't':
+                subject += ' ' + token
+            elif current == 's':
+                object_ += ' ' + token
+            elif current == 'o':
+                relation += ' ' + token
+    if subject != '' and relation != '' and object_ != '':
+        triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+
+    return triplets
+
