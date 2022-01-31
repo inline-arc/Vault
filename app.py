ea519176074307ae10edcea5ddaef50cafc7d94c
Your Name <karel.haerens@ml6.eu>
2022-01-31 10:41:45 +0100

    change

--- app.py
+++ app.py
@@ -95,11 +95,13 @@ def wiki_add_text(term):
         st.session_state['wiki_text'].append(extra_text)
         st.session_state['topics'].append(term.lower())
     except wikipedia.DisambiguationError as e:
+        print(e.error)
         with st.spinner(text="Woops, ambigious term, recalculating options..."):
             st.session_state['nodes'].remove(term)
             temp = st.session_state['nodes'] + e.options[:3]
             st.session_state['nodes'] = list(set(temp))
-    except wikipedia.WikipediaException:
+    except wikipedia.WikipediaException as e:
+        print(e.error)
         st.session_state['nodes'].remove(term)
 
 def wiki_reset_session():
