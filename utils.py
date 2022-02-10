b4f3ba3cbee574e9f5ddced17f08e61d3fee1999
Your Name <karel.haerens@ml6.eu>
2022-02-10 15:55:52 +0100

    update

--- utils.py
+++ utils.py
@@ -1,2 +1,6 @@
-def clip_text(t, lenght = 3):
-    return ".".join(t.split(".")[:lenght]) + "."
+
+def clip_text(t, lenght = 4):
+    t_sub = t.replace("...", "dotdotdot")
+    t_clipped = ".".join(t_sub.split(".")[:lenght]) + "."
+    t_reverted = t_clipped.replace("dotdotdot", "...")
+    return t_reverted
